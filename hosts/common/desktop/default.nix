a593b5bdbd367922eaeb6fc04f3fef83ff71fc04
victorvintorez <victorvintorez@proton.me>
2023-09-14 22:41:45 +0100

    fonts and seatd file rename

--- hosts/common/desktop/default.nix
+++ hosts/common/desktop/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, lib, ... }: {
   imports = [
     ./pipewire.nix
-    ./services.nix
+    # ./seatd.nix
     ./greeter.nix
   ];
 
--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -1,5 +1,22 @@
 { config, pkgs, ... }: {
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono"]; })
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts-emoji
+      (nerdfonts.override { fonts = ["Iosevka" "JetBrainsMono"]; })
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        monospace = [
+          "Iosevka Term"
+          "Iosevka Term Nerd Font Complete Mono"
+          "Iosevka Nerd Font"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [ "Iosevka Nerd Font" "Noto Color Emoji" ];
+        serif = [ "Iosevka Nerd Font" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 }

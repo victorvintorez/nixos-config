9f20ed4e0f870919ebab84f3cd7ce1e0778efcab
victorvintorez <victorvintorez@proton.me>
2023-09-25 23:49:58 +0100

    css fixes

--- home/victorvintorez/modules/desktop/gtklock.nix
+++ home/victorvintorez/modules/desktop/gtklock.nix
@@ -5,19 +5,19 @@
 
   xdg.configFile."gtklock/style.css".text = ''
     window {
-      background: rgba(0, 0, 0, .5);
+      background: rgba(0, 0, 0, .8);
       font-family: 'Iosevka Nerd Font';
     }
 
     grid > label {
       color: transparent;
-      margin: -20rem;
+      margin: -15rem;
     }
 
     button {
       all: unset;
       color: transparent;
-      margin: -20rem;
+      margin: -15rem;
     }
 
     #clock-label {
@@ -27,8 +27,7 @@
     }
 
     entry {
-      border-radius: 16px;
-      margin: 6rem;
+      border-radius: 8px;
       box-shadow: 0 1px 3px rgba(0, 0, 0, .1);
     }
   '';

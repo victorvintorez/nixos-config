8b6c3fb7b95c8e6c22b40f818ab126014d578d4e
victorvintorez <victorvintorez@proton.me>
2023-09-25 23:07:27 +0100

    fprint

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -18,6 +18,19 @@
     };
   };
 
+  security = {
+    pam = {
+      services = {
+        login = {
+          fprintAuth = true;
+        };
+        sudo = {
+          fprintAuth = true;
+        };
+      };
+    };
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

24806144ba06d00e709173b301bbc43e4ab77cbf
victorvintorez <victorvintorez@proton.me>
2023-09-25 20:54:22 +0100

    Fixes

--- home/victorvintorez/modules/desktop/ags/config/js/utils/options.js
+++ home/victorvintorez/modules/desktop/ags/config/js/utils/options.js
@@ -7,7 +7,14 @@ export default {
         medium: 50
     },
     substitutes: {
-        ClientTitle: [],
-        ClientIcon: [],
+        ClientTitle: [
+            ['codium-url-handler', 'VSCode'],
+            ['kitty', 'Terminal'],
+            ['firefox', 'Firefox'],
+            ['', 'Desktop'],
+        ],
+        ClientIcon: [
+            ['-symbolic', 'preferences-desktop-display-symbolic'],
+        ],
     }
 }

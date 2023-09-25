9fcd9c1b4aa913e693c3314253c5418916911719
victorvintorez <victorvintorez@proton.me>
2023-09-25 20:55:44 +0100

    styles

--- home/victorvintorez/modules/desktop/ags/config/config.js
+++ home/victorvintorez/modules/desktop/ags/config/config.js
@@ -1,7 +1,7 @@
 import TopBar from './js/topbar/TopBar.js'
 import { forMonitors } from './js/utils/monitor.js'
 
-const css = ags.App.configDir + '/css/style.css';
+const css = ags.App.configDir + '/css/styles.css';
 
 export default {
     style: css,

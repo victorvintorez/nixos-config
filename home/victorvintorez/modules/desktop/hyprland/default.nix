620be12b78eebe030df1ea8944b0c9af9f9557fb
victorvintorez <victorvintorez@proton.me>
2023-09-25 02:46:53 +0100

    goofy spelling mistake

--- home/victorvintorez/modules/desktop/hyprland/default.nix
+++ home/victorvintorez/modules/desktop/hyprland/default.nix
@@ -83,7 +83,7 @@
       };
       gestures = {
         workspace_swipe = true;
-        worspace_swipe_fingers = 3;
+        workspace_swipe_fingers = 3;
         workspace_swipe_distance = 300;
         workspace_swipe_invert = false;
       };

d7d93f9e1221610708c4c629367f97cafbece9fe
victorvintorez <victorvintorez@proton.me>
2023-09-26 20:49:36 +0100

    testing Aylur's config

--- home/victorvintorez/modules/desktop/ags/config/js/topbar/PanelButton.js
+++
@@ -1,9 +0,0 @@
-const { Button, Box } = ags.Widget;
-
-const PanelButton = ({ className, content, ...rest }) => Button({
-    className: `topbar-panelbutton ${className}`,
-    child: Box({ children: [content] }),
-    ...rest
-});
-
-export default PanelButton;
--- home/victorvintorez/modules/desktop/ags/config/js/utils/monitor.js
+++
@@ -1,6 +0,0 @@
-export const forMonitors = (widget, primary = null) => {
-    const ws = ags.Service.Hyprland.HyprctlGet('monitors');
-    if(!primary || typeof primary !== 'number')
-        return ws.map(mon => widget(mon.id))
-    return widget(primary)
-}

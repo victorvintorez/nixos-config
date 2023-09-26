40d1ed35470cbbf9e39bdf4d0dd17c866ea85b41
victorvintorez <victorvintorez@proton.me>
2023-09-26 20:51:24 +0100

    init swww

--- home/victorvintorez/modules/desktop/hyprland/startup.nix
+++ home/victorvintorez/modules/desktop/hyprland/startup.nix
@@ -3,6 +3,7 @@
     settings = {
       exec-once = [
         "gtklock"
+        "swww init"
         "wl-paste --type text --watch cliphist store"
         "wl-paste -p --watch wl-copy -pc"
       ];

69c71f315b8b59b1a7731660caed1f4a954a8025
victorvintorez <victorvintorez@proton.me>
2023-09-27 01:34:02 +0100

    audio.enable

--- hosts/common/desktop/pipewire.nix
+++ hosts/common/desktop/pipewire.nix
@@ -5,6 +5,9 @@
       alsa = {
         enable = true;
       };
+      audio = {
+        enable = true;
+      };
       wireplumber.enable = true;
       pulse.enable = true;
       jack.enable = true;

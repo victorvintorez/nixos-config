585c4eb7ff5b257f14901c9f7788f265d69cca76
victorvintorez <victorvintorez@proton.me>
2023-09-29 13:46:33 +0100

    autostart ags

--- home/victorvintorez/modules/desktop/hyprland/startup.nix
+++ home/victorvintorez/modules/desktop/hyprland/startup.nix
@@ -2,10 +2,10 @@
   wayland.windowManager.hyprland = {
     settings = {
       exec-once = [
-        "gtklock"
-        "swww init"
+        "ags"
         "wl-paste --type text --watch cliphist store"
         "wl-paste -p --watch wl-copy -pc"
+        "gtklock"
       ];
     };
   };

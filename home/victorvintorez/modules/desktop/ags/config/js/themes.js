ae9d0d0e8d75ba8c60c1e47785d645fd0df53a43
victorvintorez <victorvintorez@proton.me>
2023-09-29 14:36:16 +0100

    add wallpaper support

--- home/victorvintorez/modules/desktop/ags/config/js/themes.js
+++ home/victorvintorez/modules/desktop/ags/config/js/themes.js
@@ -1,19 +1,9 @@
-const WP = `/home/${ags.Utils.USER}/Pictures/Wallpapers/`;
+const WP = `/home/${ags.Utils.USER}/.config/wallpapers/`;
 
 const editScheme = (scheme, edit) => {
     const obj = {};
     Object.keys(scheme).forEach(color => obj[color] = edit(scheme[color]));
     return obj;
-};
-
-const gnome = {
-    red: '#f66151',
-    green: '#57e389',
-    yellow: '#f6d32d',
-    blue: '#62a0ea',
-    magenta: '#c061cb',
-    teal: '#5bc8aF',
-    orange: '#ffa348',
 };
 
 const charm = {
@@ -32,14 +22,6 @@ const dark = {
     fg_color: '#eee',
     hover_fg: '#f1f1f1',
     ...charm,
-};
-
-const light = {
-    color_scheme: 'light',
-    bg_color: '#fffffa',
-    fg_color: '#141414',
-    hover_fg: '#0a0a0a',
-    ...editScheme(gnome, c => `darken(${c}, 8%)`),
 };
 
 const misc = {
@@ -71,7 +53,7 @@ const colors = {
 
 // themes
 const kitty_dark = {
-    wallpaper: WP + 'kitty.jpeg',
+    wallpaper: WP + 'windows-error.jpg',
     name: 'kitty_dark',
     icon: '󰄛',
     ...dark,
@@ -79,105 +61,6 @@ const kitty_dark = {
     ...colors,
 };
 
-const kitty_light = {
-    wallpaper: WP + 'kitty.jpeg',
-    name: 'kitty_light',
-    icon: '󰄛',
-    ...light,
-    ...misc,
-    ...colors,
-    widget_bg: '$accent',
-    widget_opacity: 84,
-};
-
-const leaves_dark = {
-    ...kitty_dark,
-    wallpaper: WP + 'leaves.jpg',
-    name: 'leaves_dark',
-    icon: '󰌪',
-    accent: '$green',
-    widget_opacity: 92,
-    active_gradient: 'to right, $accent, darken($accent, 14%)',
-    border_opacity: 86,
-    bg_color: 'transparentize(#171717, 0.3)',
-    hypr_active_border: 'rgba(57e389FF)',
-    bar_style: 'floating',
-    radii: 0,
-    wallpaper_fg: 'white',
-    additional_scss: `
-        window#quicksettings .quicksettings {
-            margin-right: $wm_gaps;
-        }
-    `,
-};
-
-const leaves_light = {
-    ...leaves_dark,
-    ...light,
-    name: 'leaves_light',
-    accent: 'darken($green, 10%)',
-    bg_color: 'transparentize(#fff, 0.1)',
-    active_gradient: 'to right, $accent, darken($accent, 6%)',
-};
-
-const ivory = {
-    wallpaper: WP + 'ivory.png',
-    name: 'ivory',
-    icon: '󰟆',
-    ...light,
-    ...misc,
-    ...colors,
-    bg_color: '#fffff0',
-    bar_style: 'separated',
-    widget_bg: '$accent',
-    widget_opacity: 64,
-    drop_shadow: false,
-    border_width: 2,
-    border_opacity: 0,
-    active_gradient: 'to right, $accent, darken($accent, 6%)',
-    wallpaper_fg: '$bg_color',
-    hypr_active_border: 'rgba(111111FF)',
-    additional_scss: `
-        window#quicksettings .quicksettings,
-        window#dashboard .dashboard {
-            margin-top: 0;
-        }
-    `,
-};
-
-const cutefish = {
-    ...kitty_dark,
-    wallpaper: WP + 'cutefish.jpeg',
-    name: 'cutefish',
-    icon: '󰈺',
-    accent: 'lighten($blue, 20%)',
-    active_gradient: 'to right, $accent, lighten(mix($orange, $blue, 70%), 20%)',
-    border_opacity: 94,
-    brorder_width: 2,
-    radii: 9,
-    desktop_clock: 'end end',
-    hypr_active_border: 'rgba(3f3f3fFF)',
-    bg_color: 'transparentize(#171717, 0.3)',
-};
-
-const cherry = {
-    ...kitty_dark,
-    wallpaper: WP + 'cherry.png',
-    name: 'cherry',
-    icon: '',
-    accent: '$blue',
-    active_gradient: 'to right, $accent, lighten(mix($magenta, $blue, 70%), 18%)',
-    brorder_width: 2,
-    radii: 13,
-    bg_color: 'transparentize(#171717, 0.3)',
-};
-
 export default [
     kitty_dark,
-    kitty_light,
-    leaves_dark,
-    leaves_light,
-    ivory,
-    cutefish,
-    cherry,
 ];

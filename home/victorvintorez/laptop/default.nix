0e18f83d642e4f9630a612593ca727e69066ede2
victorvintorez <victorvintorez@proton.me>
2023-10-13 14:04:11 +0100

    fix allow unfree

--- home/victorvintorez/laptop/default.nix
+++ home/victorvintorez/laptop/default.nix
@@ -10,6 +10,13 @@
   # Add stuff for your user as you see fit:
   #programs.neovim.enable = true;
 
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }

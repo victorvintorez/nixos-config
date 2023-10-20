2bb8f9d4f58acea45e79ddc03af361c1932e6a82
victorvintorez <victorvintorez@proton.me>
2023-10-20 15:57:37 +0100

    fix

--- home/victorvintorez/modules/desktop/ags/config/js/bar/TopBar.js
+++ home/victorvintorez/modules/desktop/ags/config/js/bar/TopBar.js
@@ -60,7 +60,6 @@ const End = () => Widget.Box({
             items: submenuItems,
             children: [
                 SysTray(),
-                ColorPicker(),
             ],
         }),
         SeparatorDot(),

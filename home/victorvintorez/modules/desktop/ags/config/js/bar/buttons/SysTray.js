34897b795b6ecf20a72d0ec5016726f32185a125
victorvintorez <victorvintorez@proton.me>
2023-10-20 12:08:59 +0100

    test

--- home/victorvintorez/modules/desktop/ags/config/js/bar/buttons/SysTray.js
+++ home/victorvintorez/modules/desktop/ags/config/js/bar/buttons/SysTray.js
@@ -5,15 +5,6 @@ import Gdk from 'gi://Gdk';
 const SysTrayItem = item => PanelButton({
     content: Widget.Icon({ binds: [['icon', item, 'icon']] }),
     binds: [['tooltipMarkup', item, 'tooltip-markup']],
-    setup: btn => {
-        const id = item.menu.connect('popped-up', menu => {
-            btn.toggleClassName('active');
-            menu.connect('notify::visible', menu => {
-                btn.toggleClassName('active', menu.visible);
-            });
-            menu.disconnect(id);
-        });
-    },
     onPrimaryClick: btn =>
         item.menu.popup_at_widget(btn, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null),
     onSecondaryClick: btn =>

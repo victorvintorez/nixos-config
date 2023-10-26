fb1cc5b277b1337aafb3a3951da52b65f3ff5a6e
victorvintorez <victorvintorez@proton.me>
2023-10-26 14:41:59 +0100

    fix

--- home/victorvintorez/modules/desktop/waybar/default.nix
+++ home/victorvintorez/modules/desktop/waybar/default.nix
@@ -7,10 +7,10 @@
     style = builtins.readFile ./style.css;
   };
 
-  xdg.configFile."waybar/config".source = config.lib.file.mkOutOfStoreSymlink "${./config}";
+  xdg.configFile."waybar/config".source =  config.lib.file.mkOutOfStoreSymlink "${./config}";
 
-  xdg.configFile."scripts/randwall.sh".source = config.lib.file.mkOutOfStoreSymlink "${./randwall.sh}";
-  xdg.configFile."scripts/volume".source = config.lib.file.mkOutOfStoreSymlink "${./volume}";
+  xdg.configFile."scripts/randwall.sh".source = pkgs.writeShellScript "randwall" builtins.readFile ./randwall.sh;
+  xdg.configFile."scripts/volume.sh".source = pkgs.writeShellScript "volume" builtins.readFile ./volume.sh;
 
   home.packages = with pkgs; [
     playerctl

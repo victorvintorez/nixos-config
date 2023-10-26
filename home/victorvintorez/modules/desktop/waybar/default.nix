bd5cada6cc24cfc7a4f4eb3922b10637f2c06ad3
victorvintorez <victorvintorez@proton.me>
2023-10-26 15:23:26 +0100

    fix

--- home/victorvintorez/modules/desktop/waybar/default.nix
+++ home/victorvintorez/modules/desktop/waybar/default.nix
@@ -1,6 +1,39 @@
 { config, pkgs, lib, ... }: let 
-  randwall = (pkgs.writeShellScriptBin "randwall" ./randwall.sh).outPath;
-  volume = (pkgs.writeShellScriptBin "volume" ./volume.sh).outPath;
+  randwall = (pkgs.writeShellScript "randwall" ''
+    export SWWW_TRANSITION_FPS=61
+    export SWWW_TRANSITION_STEP=2
+
+    find "~/.config/wallpapers" |
+	    shuf -n 1 | while read -r img; do ${pkgs.swww} img "$img" -o eDP-1; done
+  '').outPath;
+  volume = (pkgs.writeShellScript "volume" ''
+    down() {
+      ${pkgs.pamixer} -d 5
+      volume=$(${pkgs.pamixer} --get-volume)
+      [$volume -gt 0 ] && volume=`expr $volume`  
+    }
+
+    up() {
+      ${pkgs.pamixer} -i 5
+      volume=$(${pkgs.pamixer} --get-volume)
+      [ $volume -lt 100 ] && volume=`expr $volume`  
+    }
+
+    mute() {
+      muted="$(${pkgs.pamixer} --get-mute)"
+      if $muted; then
+        ${pkgs.pamixer} -u
+      else 
+        ${pkgs.pamixer} -m
+      fi
+    }
+
+    case "$1" in
+      up) up;;
+      down) down;;
+      mute) mute;;
+    esac
+  '').outPath;
 in {
   programs.waybar = {
     enable = true;
@@ -18,6 +51,6 @@ in {
   home.packages = with pkgs; [
     playerctl
     networkmanagerapplet
-    pamixer
+    pkgs.pamixer
   ];
 }

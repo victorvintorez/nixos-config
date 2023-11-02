af7e9aaf3f9382388f25592f89d5f4b5b8251f76
victorvintorez <victorvintorez@proton.me>
2023-11-02 22:49:16 +0000

    fix win

--- home/victorvintorez/modules/desktop/hyprland/windowrules.nix
+++ home/victorvintorez/modules/desktop/hyprland/windowrules.nix
@@ -8,6 +8,10 @@
         "nofocus, class:^(xwaylandvideobridge)$"
         "noinitialfocus, class:^(xwaylandvideobridge)$"
 
+        # Fix for jetbrains spawning windows all the time
+        "noanim, class:^(xwaylandvideobridge)$"
+        "noinitialfocus, class:^(win)*.$"
+
         # Floating windows by default
         "float, class:^(org.kde.polkit-kde-authentication-agent-1)$"
         "float, class:^(pavucontrol)$"

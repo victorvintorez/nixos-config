9ea23165904b42d9c9c5bfa897279ec4dc7675b7
victorvintorez <victorvintorez@proton.me>
2023-11-10 16:55:11 +0000

    fix

--- home/victorvintorez/modules/desktop/hyprland/startup.nix
+++ home/victorvintorez/modules/desktop/hyprland/startup.nix
@@ -2,6 +2,7 @@
   wayland.windowManager.hyprland = {
     settings = {
       exec-once = [
+        "swww init"
         "wl-paste --type text --watch cliphist store"
         "wl-paste -p --watch wl-copy -pc"
         "gtklock"

320a87a3f781aabb8dee8b4472103a60f14d588e
victorvintorez <victorvintorez@proton.me>
2023-11-28 00:55:50 +0000

    quick fix

--- modules/monitors.nix
+++ modules/monitors.nix
@@ -70,7 +70,7 @@ in
         };
         # Default Workspace for Monitor (optional)
         workspace = mkOption {
-          type = types.oneOf [ types.null types.int types.str ];
+          type = types.nullOr types.oneOf [ types.int types.str ];
           default = null;
         };
       };

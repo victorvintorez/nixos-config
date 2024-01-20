e0d7c239e3696f34a5bb127da595a3f5827b4206
victorvintorez <victorvintorez@proton.me>
2024-01-20 14:16:48 +0000

    add printer drivers

--- hosts/common/printing.nix
+++ hosts/common/printing.nix
@@ -2,6 +2,11 @@
   services = {
     printing = {
       enable = true;
+      drivers = with pkgs; [
+        brlaser
+        brgenml1lpr
+        brgenml1cupswrapper
+      ];
     };
     avahi = {
       enable = true;

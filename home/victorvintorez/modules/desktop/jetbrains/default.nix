ee192e515f44e2745f7329b1e974427ed1f9dcc3
victorvintorez <victorvintorez@proton.me>
2024-04-25 23:11:02 +0100

    add copilot fix

--- home/victorvintorez/modules/desktop/jetbrains/default.nix
+++ home/victorvintorez/modules/desktop/jetbrains/default.nix
@@ -7,6 +7,7 @@
     rider
     rust-rover
     webstorm
+    github-copilot-intellij-agent # ln -fs /run/current-system/sw/bin/copilot-agent ~/.local/share/JetBrains/IntelliJIdea2022.2/github-copilot-intellij/copilot-agent/bin/copilot-agent-linux
   ];
 
   home.sessionVariables = {

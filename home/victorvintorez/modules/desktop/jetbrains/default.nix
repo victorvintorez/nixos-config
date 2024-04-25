dddec669de55834de112a1c0e88e9842960d97d2
victorvintorez <victorvintorez@proton.me>
2024-04-25 23:15:03 +0100

    fix

--- home/victorvintorez/modules/desktop/jetbrains/default.nix
+++ home/victorvintorez/modules/desktop/jetbrains/default.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }: {
-  home.packages = with pkgs.jetbrains; [
-    clion
-    datagrip
-    idea-ultimate
-    pycharm-professional
-    rider
-    rust-rover
-    webstorm
+  home.packages = with pkgs; [
+    jetbrains.clion
+    jetbrains.datagrip
+    jetbrains.idea-ultimate
+    jetbrains.pycharm-professional
+    jetbrains.rider
+    jetbrains.rust-rover
+    jetbrains.webstorm
     github-copilot-intellij-agent # ln -fs /run/current-system/sw/bin/copilot-agent ~/.local/share/JetBrains/IntelliJIdea2022.2/github-copilot-intellij/copilot-agent/bin/copilot-agent-linux
   ];
 
